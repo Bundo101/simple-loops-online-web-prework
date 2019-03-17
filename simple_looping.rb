454ff98e9a5898fb0c61b2ee0b400ead3e743a0a
James Britton <brittonjp@gmail.com>
2019-03-17 18:01:06 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,9 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   loop do
-    
+    counter += 1
+    puts phrase
+    if counter == 7
 end
 
 def times_iterator(number_of_times)
